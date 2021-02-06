66386ab7379ccd2c1725d0eff6fabff95bd07839
soumya <soumya.d.mohanty@gmail.com>
2021-02-05 19:59:23 -0600

    Added spectrogram

--- SIGNALS/testcrcbgenqcsig.m
+++ SIGNALS/testcrcbgenqcsig.m
@@ -37,3 +37,16 @@ fftSig = fftSig(1:kNyq);
 %Plot periodogram
 figure;
 plot(posFreq,abs(fftSig));
+
+%Plot a spectrogram
+%----------------
+winLen = 0.2;%sec
+ovrlp = 0.1;%sec
+%Convert to integer number of samples 
+winLenSmpls = floor(winLen*samplFreq);
+ovrlpSmpls = floor(ovrlp*samplFreq);
+[S,F,T]=spectrogram(sigVec,winLenSmpls,ovrlpSmpls,[],samplFreq);
+figure;
+imagesc(T,F,abs(S)); axis xy;
+xlabel('Time (sec)');
+ylabel('Frequency (Hz)');
