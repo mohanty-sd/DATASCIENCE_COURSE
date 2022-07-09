49769d81ad4eb246507a51abea6d1bccd82c28cd
Soumya D. Mohanty <soumya.mohanty@utrgv.edu>
2022-07-09 17:07:16 -0500

    Changed signal.

--- DSP/FTExample.m
+++ DSP/FTExample.m
@@ -1,10 +1,11 @@
 %% Simple example of a signal and its Fourier transform
 %%
-% Time domain signal: sum of two sinusoids with frequencies 5 Hz and 1 Hz
+% Time domain signal
 nSamples = 20480;
 samplingFreq = 100;
 t = (0:(nSamples-1))/samplingFreq;
-st = sin(2*pi*2*t).*(sin(2*pi*5*t)+2*sin(2*5.5*pi*t));
+st = sin(2*pi*5*t)+2*sin(2*pi*5.5*t);
+%st = sin(2*pi*2*t).*(sin(2*pi*5*t)+2*sin(2*5.5*pi*t));
 % soundsc(st);
 % audiowrite('SampleSoundSignal.wav',st,8192);
 %%
