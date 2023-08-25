ed2c486b8269b9087583ac7f14d6b21a8a007421
Soumya D. Mohanty <soumya.mohanty@utrgv.edu>
2023-08-25 01:20:05 -0500

    Updates in Zhuhai school

--- DETEST/GLRTcalc.m
+++ DETEST/GLRTcalc.m
@@ -24,15 +24,20 @@ posFreq = (0:(kNyq-1))*(1/dataLen);
 psdPosFreq = noisePSD(posFreq);
 
 %% Generate  data realization
-% Noise + SNR=10 signal. 
+% Parameters of the signal to be injected. 
 a1=9.5;
 a2=2.8;
 a3=3.2;
-A=10;
+A=2; %SNR
+%The signal will be normalized, so generate with an arbitrary amplitude
+sig4data = crcbgenqcsig(timeVec,1,[a1,a2,a3]);
+% Signal normalized to SNR = A in noise with the specified PSD
+[sig4data,~]=normsig4psd(sig4data,sampFreq,psdPosFreq,A);
+
+%Generate a noise realization from a stochastic process with the specified PSD
 noiseVec = statgaussnoisegen(nSamples,[posFreq(:),psdPosFreq(:)],100,sampFreq);
-sig4data = crcbgenqcsig(timeVec,A,[a1,a2,a3]);
-% Signal normalized to SNR=10
-[sig4data,~]=normsig4psd(sig4data,sampFreq,psdPosFreq,10);
+
+%Data realization = noise realization + signal
 dataVec = noiseVec+sig4data;
 
 figure;
@@ -53,7 +58,7 @@ plot(posFreq,datFFT(1:kNyq));
 hold on;
 plot(posFreq,sigFFT(1:kNyq));
 xlabel('Frequency (Hz)');
-ylabel('Periodogram');
+ylabel('Periodogram of data');
 
 figure;
 [S,F,T] = spectrogram(dataVec,64,60,[],sampFreq);
@@ -62,16 +67,56 @@ xlabel('Time (sec)')
 ylabel('Frequency (Hz)');
 
 %% Compute GLRT
-%Generate the unit norm signal (i.e., template). Here, the value used for
+%Generate the unit norm signal (i.e., template). As before, the value used for
 %'A' does not matter because we are going to normalize the signal anyway.
 %Note: the GLRT here is for the unknown amplitude case, that is all other
 %signal parameters are known
-sigVec = crcbgenqcsig(timeVec,A,[a1,a2,a3]);
-%We do not need the normalization factor, just the  template vector
+sigVec = crcbgenqcsig(timeVec,1,[a1,a2,a3]);
+%We do not need the normalization factor, just the  template vector with unit norm.
 [templateVec,~] = normsig4psd(sigVec,sampFreq,psdPosFreq,1);
-% Calculate inner product of data with template
+% Calculate inner product of the data with the unit norm template
 llr = innerprodpsd(dataVec,templateVec,sampFreq,psdPosFreq);
 %GLRT is its square
 llr = llr^2;
 disp(llr);
+
+%% Estimate the distribution of GLRT under the null and alternative hypotheses
+% Number of data realizations to generate under each hypothesis
+nRlz = 500;
+%GLRT values stored for each realization
+glrtH0 = zeros(1,nRlz); %Null hypothesis
+glrtH1 = zeros(1,nRlz); %Alternative hypothesis
+
+%Always a good idea to reset the random number generator when doing large
+%simulations for later reproducibility
+rng('default');
+
+%Calculate GLRT under null hypothesis: data is pure noise
+for lpr = 1:nRlz
+    %Generate noise realization
+    noiseVec = statgaussnoisegen(nSamples,[posFreq(:),psdPosFreq(:)],100,sampFreq);
+    %Compute GLRT
+    llr = innerprodpsd(noiseVec,templateVec,sampFreq,psdPosFreq);
+    glrtH0(lpr) = llr^2;
+end
+
+%Calculate GLRT under alternative hypothesis: data contains the signal
+%generated above.
+for lpr = 1:nRlz
+    %Generate noise realization
+    noiseVec = statgaussnoisegen(nSamples,[posFreq(:),psdPosFreq(:)],100,sampFreq);
+    %Add signal
+    dataVec = noiseVec+sig4data;
+    %Compute GLRT
+    llr = innerprodpsd(dataVec,templateVec,sampFreq,psdPosFreq);
+    glrtH1(lpr) = llr^2;
+end
+
+%Plot histograms
+figure;
+hold on;
+histogram(glrtH0,'Normalization',"pdf");
+histogram(glrtH1,'Normalization',"pdf")
+legend('H0', 'H1');
+xlabel('GLRT');
 
